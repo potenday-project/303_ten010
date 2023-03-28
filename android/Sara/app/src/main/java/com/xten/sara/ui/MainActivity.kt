350c2877934183242f14b24a147aa0573521c165
SANDY-9 <nnal0256@naver.com>
2023-03-28 11:55:36 +0900

    기본 UI 컴포넌트 생성, theme 설정

--- android/Sara/app/src/main/java/com/xten/sara/ui/MainActivity.kt
+++ android/Sara/app/src/main/java/com/xten/sara/ui/MainActivity.kt
@@ -2,11 +2,33 @@ package com.xten.sara.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.xten.sara.R
+import com.xten.sara.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
     }
+
+    private fun initBottomNavigation() = binding.navBottom.apply {
+        setupWithNavController(getNavController())
+        setOnItemReselectedListener { /* NO-OP */ }
+    }
+    private fun getNavController() : NavController {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
+    }
+
 }
---
+++ android/Sara/app/src/main/java/com/xten/sara/ui/my/MyFragment.kt
@@ -0,0 +1,20 @@
+package com.xten.sara.ui.my
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.xten.sara.R
+
+class MyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my, container, false)
+    }
+
+}
